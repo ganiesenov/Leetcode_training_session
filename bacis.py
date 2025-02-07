32e0f81d91fa9a181ae9e78836919948d39f656b
ganiesenov <ganiesenov@gmail.com>
2025-02-07 16:15:19 +0500

    123

---
+++ bacis.py
@@ -0,0 +1,14 @@
+
+# Output: [0,1]
+class Solution:
+    def twoSum(self, nums: list, target: int) -> list:
+        for i in range(len(nums)): 
+            for j in range(len(nums)): 
+                if i!=j and  nums[i] + nums[j] == target:
+                    return[i,j]
+        return []
+    
+nums = [2,7,11,15]
+target = 9
+solution = Solution()
+print(solution.twoSum(nums, target))
