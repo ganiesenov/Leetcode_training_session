2ce77e1e504223fc64eb225d53c6f8b7775f9097
ganiesenov <ganiesenov@gmail.com>
2025-02-11 14:38:07 +0500

    enumerate

--- TwoSum_Hashmap.py
+++ TwoSum_Hashmap.py
@@ -3,12 +3,12 @@ nums = [2,7,11,15]
 target = 26
 class Solution:
     def twoSum(self, nums: list, target: int) -> list:
-       hashmap = {}
-       for i in range(len(nums)):
-           complement = target - nums[i]
-           if complement in hashmap: 
-               return [hashmap[complement], i]
-           hashmap[nums[i]] == i
+        hashmap = {}
+        for i, k in enumerate(nums):
+            complement = target - nums[i] 
+            if complement in hashmap:           
+                return [hashmap[complement], i]
+            hashmap[k] = i     
    
    
 solution = Solution()
